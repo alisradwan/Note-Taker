96d49933836ff6c5636e9188bbb07edd9e5e3f07
alisradwan <aloshradwan44@gmail.com>
2022-10-27 14:42:23 -0700

    trying figring out new stuff

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
+const htmlRouters = require("./router/htmlRouters");
+const apiRoutes = require("./router/ApiRouter");
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
 app.use(express.static("public"));
+app.use("/", htmlRouters);
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
