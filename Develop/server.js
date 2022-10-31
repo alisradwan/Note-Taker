f7df2ca660b38eaed683ec2c3fe1d12a8284be2a
alisradwan <aloshradwan44@gmail.com>
2022-10-31 16:47:27 -0700

    made changese with the api

--- Develop/server.js
+++ Develop/server.js
@@ -4,13 +4,13 @@ const fs = require("fs");
 const htmlRouters = require("./router/htmlRouters");
 const apiRoutes = require("./router/ApiRouter");
 
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRouters);
